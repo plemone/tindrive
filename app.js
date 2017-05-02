691dd23754d0d9dacebce5f67e06cd0670e5843b
root <tanvir.tinz@gmail.com>
2017-05-02 02:13:05 -0400

    lost so much work...

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 	Author: Md. Tanvir Islam
 */
 
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-const ROOT = "./static";
-
+var express = require("express"); // importing express module
+var app = express(); // an instance of the express server
+var bodyParser = require("body-parser"); // body parser to parse the body of post requests
+var MongoClient = require("mongodb").MongoClient; // database module
+var bcrypt = require("bcrypt"); // password encryption module
+const ROOT = "./"; // Root directory
+const SALT = 10;
 
 // binding middlewares
 
@@ -21,12 +23,74 @@ app.use(function(req, res, next) {
 	next();
 });
 
-
+// routing
 app.get("/", function(req, res) {
 	res.status(200).render("index");
 });
 
+app.post("/authenticate", function(req, res) {
+	MongoClient.connect("mongodb://localhost:27017/TinDrive", function(err, db) {
+		if (err) console.log("Failed to connect to TinDrive database...");
+		else {
+			console.log("Access to TinDrive database successful...");
+			var collection = db.collection("Users");
+			if (req.body.which == "#login") {
+				// handle login
+
+			} else {
+				// handle registration
+				var userDetails = {}; 
+				// always use find one if you are absolutely sure that there is one document you are looking for
+				collection.findOne({"name": req.body.username}, function(err, doc) {
+					if (doc != null && doc.name === req.body.username) {
+						res.status(200).send("0");
+						db.close();
+					} else {
+						bcrypt.genSalt(SALT, function(err, salt) {
+							bcrypt.hash(req.body.password, salt, function(err, hash) {
+								userDetails.password = hash;
+								collection.insert(userDetails);
+								// closes the opened database to make sure data gets saved
+								db.close(); 
+								// this function body is the last thing that gets executed in this funciton body
+								res.status(200).send("1");
+							});
+						});
+						// asynchronousity allows this to run first
+						userDetails.name = req.body.username;
+					}
+				});
+			}
+		}
+	});
+});
+
+
+app.post("/nameCheck", function(req, res) {
+	MongoClient.connect("mongodb://localhost:27017/TinDrive", function(err, db) {
+		if (err) console.log("Failed to connect to TinDrive databse...");
+		else {
+			console.log("Access to TinDrive database successful...");
+			db.collection("Users").findOne({"name": req.body.val}, function(err, doc) {
+				if (doc == null) { // if the name is not found in the data base then green light
+					res.status(200).send("1");
+					db.close();
+				} else { // if it is found in the database then red light
+					res.status(200).send("0");
+					db.close();
+				}
+			});
+		}
+	});
+});
+
+
+app.get("*", function(req, res) {
+	res.status(404).render("404");
+});
 
 app.listen(3000, function() {
 	console.log("Server is listening on port 3000...");
 })
+
+
--- static/js/drive.js
+++ static/js/drive.js
@@ -2,159 +2,184 @@
 Author: Md. Tanvir Islam
 */
 $(document).ready(function() {
-    main();
+	main();
 });
 
 
 function main() {
-    var componentOne = new LoginRegistration();
-    componentOne.createMainDiv();
+	var componentOne = new LoginRegistration();
+	componentOne.createMainDiv();
 
 }
 
 class LoginRegistration {
 
-    constructor() {
-        this.expanded = false;
-        this.clicks = 0;
-    }
+	constructor() {
+		this.expanded = false;
+		this.clicks = 0;
+	}
 
-    createMainDiv() {
-        var self = this;
-        $("body").append("<div class = movable id = login></div>");
-        $("body").append("<div class = movable id = register></div>");
-        this.cssMainDiv();
-        $("#login").append("<h2 id = l style = 'color: white; text-shadow: 1px 0 black, 0 0px black, 0px 0 black, 0 1px black;'>Login</h2>")
-        $("#register").append("<h2 id = r style = 'color: white; text-shadow: 1px 0 black, 0 0px black, 0px 0 black, 0 1px black;'>Register</h2>")
+	createMainDiv() {
+		var self = this;
+		$("body").append("<div class = movable id = login></div>");
+		$("body").append("<div class = movable id = register></div>");
+		$("#login").append("<h2 id = l style = 'color: white; text-shadow: 1px 0 black, 0 0px black, 0px 0 black, 0 1px black;'>Login</h2>")
+		$("#register").append("<h2 id = r style = 'color: white; text-shadow: 1px 0 black, 0 0px black, 0px 0 black, 0 1px black;'>Register</h2>")
+		this.cssMainDiv();
+		// attaches event handler
+		$("#register").on("click", function() {
+			++self.clicks;
+			self.extend("#register");
+		});
+		// attaches event handler
+		$("#login").on("click", function() {
+			++self.clicks;
+			self.extend("#login");
+		});
+	}
 
-        // attaches event handler
-        $("#register").on("click", function() {
-            ++self.clicks;
-            self.extend("#register");
-        });
-        // attaches event handler
-        $("#login").on("click", function() {
-            ++self.clicks;
-            self.extend("#login");
-        });
-    }
+	extend(divId) {
+		// if expanded is false it means we have not clicked on the div yet so expand the div and attach the event handler
+		// to the cross div that gets dynamically added
+		var self = this;
+		if (!this.expanded && this.clicks % 2 != 0) {
+			if (divId == "#login") $(divId).animate({right: '300px', top: '200px', height: '420px', width: '400px'});
+			else $(divId).animate({left: '580px', top: '200px', height: '420px', width: '400px'});
+			$(divId).css("z-index", "1"); // z-index specifies the stack order of an element
+											// an element with a higher stack order is always on top the other element
+			$(divId).append("<img id = close-icon src = 'static/imgs/close.png'></img>");
 
-    extend(divId) {
-        console.log(this.clicks);
-        // if expanded is false it means we have not clicked on the div yet so expand the div and attach the event handler
-        // to the cross div that gets dynamically added
-        var self = this;
-        if (!this.expanded && this.clicks % 2 != 0) {
-            if (divId == "#login") $(divId).animate({right: '300px', top: '200px', height: '420px', width: '400px'});
-            else $(divId).animate({left: '580px', top: '200px', height: '420px', width: '400px'});
-            $(divId).css("z-index", "1"); // z-index specifies the stack order of an element
-                                            // an element with a higher stack order is always on top the other element
-            $(divId).append("<img id = close-icon src = 'static/imgs/close.png'></img>");
+			this.createTextFields(divId);
 
-            this.createTextFields(divId);
-
-            $("#close-icon").css("height", "5%").css("width", "5%").css("position", "relative").css("bottom", "50px").css("left", "175px");
+			$("#close-icon").css("height", "5%").css("width", "5%").css("position", "relative").css("bottom", "50px").css("left", "175px");
 
 
-            // attaches the event handler to the cross icon
-            $("#close-icon").on("click", function() {
-                self.clicks = 1; // reset clicks to 1 which is an odd number everytime you click on the cross, this prevents it from restarting
-                                 // as one of the condition is that you only expand if and only if it is even so the next time you click
-                                // has to be the time when it is actually even as 1 + 1 = 2, and when you lick again it will be 3 allowing this
-                                // function to be executed
-                if (divId == "#login") {
-                    $(divId).animate({left: '600px', top: '250px', height: '70px', width: '150'});
-                    $(divId).css("z-index", "0"); // z-index specifies the stack order of an element
-                                                // an element with a higher stack order is always on top the other element
-                } else {
-                    $(divId).animate({left: '800px', top: '250px', height: '70px', width: '150'});
-                    $(divId).css("z-index", "0"); // z-index specifies the stack order of an element
-                }
-                $("input").remove();
-                $("button").remove();
-                $("#close-icon").remove(); // the close icon should be also responsible for deleting itself and should be inside the body
-                                           // of the close event handler
-                self.expanded = false;
-            }); // attaching event handler
-            this.expanded = true;
-        }
-    }
+			// attaches the event handler to the cross icon
+			$("#close-icon").on("click", function() {
+				self.clicks = 1; // reset clicks to 1 which is an odd number everytime you click on the cross, this prevents it from restarting
+								 // as one of the condition is that you only expand if and only if it is even so the next time you click
+								// has to be the time when it is actually even as 1 + 1 = 2, and when you lick again it will be 3 allowing this
+								// function to be executed
+				self.collapse(divId);
+				self.expanded = false;
+			}); // attaching event handler
+			this.expanded = true;
+		}
+	}
 
-    createTextFields(divId) {
-        $(divId).append("<div><input class = box id = name type = text placeholder = '              username'></input><div>");
-        $(divId).append("<div><input class = box id = password type = password placeholder = '              password'></input><div>");
-        $(divId).append("<button id = submit>submit</button>");
-        this.cssText();
-        this.cssButtonSubmit();
-    }
+	collapse(divId) {
+		if (divId == "#login") {
+			$(divId).animate({left: '600px', top: '250px', height: '200px', width: '200'});
+			$(divId).css("z-index", "0"); // z-index specifies the stack order of an element
+										// an element with a higher stack order is always on top the other element
+		} else {
+			$(divId).animate({left: '800px', top: '250px', height: '200px', width: '200'});
+			$(divId).css("z-index", "0"); // z-index specifies the stack order of an element
+		}
+		$("input").remove();
+		$("button").remove();
+		$("#close-icon").remove(); // the close icon should be also responsible for deleting itself and should be inside the body
+								   // of the close event handler
+	}
 
-    cssText() {
-        $(".box").css("border-radius", "0%"); // no circular boxes, edgy boxes only
-        $(".box").css("position", "relative"); // movable element
-        $(".box").css("height", "30px").css("width", "50%").css("font-size", "18px");
-        $(".box").css("background", "transparent"); // makes the background transparent of the text boxes
-        $(".box").css("color", "white").css("border", "none"); // color of the text is white and contains no borders
-        $(".box").css("border-bottom", "1px solid white"); // only contains a bottom border which is white
-        $(".box").focusin(function() { $(this).attr("placeholder", "");}); // adds a placeholder name when focused in
-        $("#name").focusout(function() { $(this).attr("placeholder", "              username"); });
-        $("#password").focusout(function() { $(this).attr("placeholder", "              password");});
-        $("#name").css("top", "20px"); // changing position
-        $("#password").css("top", "50px"); // changing position
-    }
 
-    cssButtonSubmit() {
-        // position and dimension change
-        $("#submit").css("position", "relative");
-        $("#submit").css("top", "110px");
-        $("#submit").css("border-radius", "0px");
-        $("#submit").css("height", "45px");
-        $("#submit").css("width", "200px");
-        // looks and definition
-        $("#submit").css("background", "white");
-        $("#submit").css("box-shadow", "rgba(255,255,255,0.4) 0 0px 0, inset rgba(255,255,255,0.4) 0 0px 0");
-        $("#submit").css("text-shadow", "#545759 0 1px 0");
-        $("#submit").css("color", "#548db3");
-        $("#submit").css("font-size", "23px");
-        $("#submit").css("font-family", "helvetica, serif");
-        $("#submit").css("text-decoration", "none");
-        $("#submit").css("vertical-align", "middle");
-        $("#submit").css("cursor", "pointer"); // changes the cursor to a pointer
 
-        // on mouse over change properties
-        $("#submit").on("mouseover", function() {
-            $("#submit").css("border", "0px solid #4e565e");
-            $("#submit").css("text-shadow", "#8c9aa3 0 1px 0");
-            $("#submit").css("background", "#3e779d");
-            $("#submit").css("border", "0px solid #4e565e");
-            $("#submit").css("color", "#ebebeb");
-        });
+	createTextFields(divId) {
+		$(divId).append("<div><input class = box id = name type = text placeholder = '              username'></input><div>");
+		$(divId).append("<div><input class = box id = password type = password placeholder = '              password'></input><div>");
+		$(divId).append("<button id = submit>submit</button>");
+		this.cssText();
+		this.cssButtonSubmit();
+		this.submitForm(divId);
+	}
 
-        // on mouse out change property back to default
-        $("#submit").on("mouseout", function() {
-            $("#submit").css("border", "none");
-            $("#submit").css("text-shadow", "#545759 0 1px 0");
-            $("#submit").css("background", "white");
-            $("#submit").css("color", "#548db3");
-        });
-    }
+	submitForm(divId) {
+		$("#submit").on("click", function() {
+			var dataToSend = {};
+			dataToSend.username = $("#name").val();
+			dataToSend.password = $("#password").val();
+			dataToSend.which = divId;
+			$.ajax({
+				url: "/authenticate",
+				type: "POST",
+				data: dataToSend,
+			})
+		})
+	}
 
-    cssMainDiv() {
-        var cls = ".movable";
-        var login = "#login";
-        var register = "#register";
-        $(cls).css("text-align", "center");
-        $(cls).css("border", "1px black solid");
-        $(cls).css("position", "fixed");
-        $(cls).css("background", "#333333");
-        $(cls).css("box-shadow", "2px 0px 4px grey"); // adds a shadow to the box divs
-        $(cls).css("height", "70px");
-        $(cls).css("width", "150px");
-        $(cls).css("border-radius", "2%");
-        $(cls).css("cursor", "pointer"); // changes the cursor on hover
-        $(login).css("left", "600px");
-        $(login).css("top", "250px");
-        $(register).css("left", "800px");
-        $(register).css("top", "250px");
-    }
+	cssText() {
+		$(".box").css("border-radius", "0%"); // no circular boxes, edgy boxes only
+		$(".box").css("position", "relative"); // movable element
+		$(".box").css("height", "30px").css("width", "50%").css("font-size", "18px");
+		$(".box").css("background", "transparent"); // makes the background transparent of the text boxes
+		$(".box").css("color", "white").css("border", "none"); // color of the text is white and contains no borders
+		$(".box").css("border-bottom", "1px solid white"); // only contains a bottom border which is white
+		$(".box").focusin(function() { $(this).attr("placeholder", "");}); // adds a placeholder name when focused in
+		$("#name").focusout(function() { $(this).attr("placeholder", "              username"); });
+		$("#password").focusout(function() { $(this).attr("placeholder", "              password");});
+		$("#name").css("top", "20px"); // changing position
+		$("#password").css("top", "50px"); // changing position
+	}
+
+	cssButtonSubmit() {
+		// position and dimension change
+		$("#submit").css("position", "relative");
+		$("#submit").css("top", "110px");
+		$("#submit").css("border-radius", "0px");
+		$("#submit").css("height", "45px");
+		$("#submit").css("width", "200px");
+		// looks and definition
+		$("#submit").css("background", "white");
+		$("#submit").css("box-shadow", "rgba(255,255,255,0.4) 0 0px 0, inset rgba(255,255,255,0.4) 0 0px 0");
+		$("#submit").css("text-shadow", "#545759 0 1px 0");
+		$("#submit").css("color", "#548db3");
+		$("#submit").css("font-size", "23px");
+		$("#submit").css("font-family", "helvetica, serif");
+		$("#submit").css("text-decoration", "none");
+		$("#submit").css("vertical-align", "middle");
+		$("#submit").css("cursor", "pointer"); // changes the cursor to a pointer
+
+		// on mouse over change properties
+		$("#submit").on("mouseover", function() {
+			$("#submit").css("border", "0px solid #4e565e");
+			$("#submit").css("text-shadow", "#8c9aa3 0 1px 0");
+			$("#submit").css("background", "#3e779d");
+			$("#submit").css("border", "0px solid #4e565e");
+			$("#submit").css("color", "#ebebeb");
+		});
+
+		// on mouse out change property back to default
+		$("#submit").on("mouseout", function() {
+			$("#submit").css("border", "none");
+			$("#submit").css("text-shadow", "#545759 0 1px 0");
+			$("#submit").css("background", "white");
+			$("#submit").css("color", "#548db3");
+		});
+	}
+
+	cssMainDiv() {
+		var cls = ".movable";
+		var login = "#login";
+		var register = "#register";
+		var l = "#l";
+		var r = "#r";
+		$(cls).css("background-image", "url(static/imgs/cloud-3.png)");
+		$(cls).css("background-repeat", "no-repeat");
+		$(cls).css("background-size", "contain");		
+		$(cls).css("text-align", "center");
+		$(cls).css("border", "none");
+		$(cls).css("position", "fixed");
+		$(cls).css("height", "200px");
+		$(cls).css("width", "200px");
+		$(cls).css("top", "250px");
+		$(login).css("left", "600px");
+		$(register).css("left", "800px");
+		$(cls).css("cursor", "pointer"); // changes the cursor on hover	
+		$(l).css("position", "relative");
+		$(r).css("position", "relative");
+		$(l).css("top", "80px");
+		$(r).css("top", "80px");	
+	}
 
 }
+
