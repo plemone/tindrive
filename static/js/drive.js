017ff9216e222c79839e0087a5361b8bc606782e
root <tanvir.tinz@gmail.com>
2017-05-04 17:37:28 -0400

    dragndrop! div created

--- static/js/drive.js
+++ static/js/drive.js
@@ -9,7 +9,35 @@ $(document).ready(function() {
 
 function main() {
 	var logoutButton = new Logout();
+	var dragDrop = new DropBox();
 	logoutButton.init();
+	dragDrop.init();
+}
+
+
+
+
+
+class DropBox {
+
+	init() {
+		$("body").append("<div id = main-div></div");
+		this.generateCSS();
+	}
+
+	generateCSS() {
+		var id = "#main-div";
+		$(id).css("position", "relative");
+		$(id).css("height", "600px");
+		$(id).css("width", "1000px");
+		$(id).css("left", "280px");
+		$(id).css("bottom", "20px");
+		$(id).css("border", "white");
+		$(id).css("background", "white");
+		$(id).css("border-radius", "2%")
+
+	}
+
 }
 
 
