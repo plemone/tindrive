80a2a5366f424483de772c471b381eaa6699d538
root <tanvir.tinz@gmail.com>
2017-05-21 00:57:46 -0400

    commiter script added too avoid a lot of typing

--- git.py
+++ git.py
@@ -8,9 +8,13 @@ def git_commit(details):
 	os.system("sudo git push")
 
 def main():
+	# OptionParser object instantiated and aliased by the variable parser
 	parser = optparse.OptionParser("-m <commit>")
+	# options being added to the parser object
 	parser.add_option("-m", dest = "commit", type = "string", help = "commit description")
-	(options, args) = parser.parse_args()
+	# obtained the tuple returns a tuple containing two elements, we are only concerned with the
+	# the first element options which is an object containing out variable commit which can be accessed
+	(options, _) = parser.parse_args()
 
 	if options.commit == None:
 		# print the error message saying that the user failed to follow instructions
