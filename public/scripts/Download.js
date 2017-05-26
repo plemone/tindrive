f2c290ed4a1da48f0e372833138541183919481e
root <tanvir.tinz@gmail.com>
2017-05-26 15:26:42 -0400

    button effects!

--- public/scripts/Download.js
+++ public/scripts/Download.js
@@ -3,14 +3,12 @@
 class Download {
 
 	constructor() {
-		this.x = 890; // contains x coordinate
-		this.y = -570; // contains y ccoordinate
-		this.width = 43; // width of the button
-		this.height = 33; // height of the button
+		this.x = 827; // contains x coordinate
+		this.y = -600; // contains y ccoordinate
+		this.width = 174; // width of the button
+		this.height = 45; // height of the button
 		this.id = "#download"; // id of the object
-		this.element = "<div id = download></div>"; // contains the DOM information
-		this.background = "url(public/images/download-2.png)"; // background image
-		this.changeBackground = "url(public/images/download-1.png)"; // the image which background image changes to on hover
+		this.element = "<div id = download><p id = description >download</p></div>"; // contains the DOM information
 		this.contents = []; // contains the contents that need to be downloaded
 	}
 
@@ -22,19 +20,39 @@ class Download {
 
 	attachEH() {
 		var self = this;
+
 		$(this.id).on("mouseover", function() {
-			$(self.id).css("background-image", self.changeBackground);
+			$(self.id).css("box-shadow", "0 4px 2px -2px gray");
 		});
+
 
 		$(this.id).on("mouseout", function() {
-			$(self.id).css("background-image", self.background);
+			$(self.id).css("box-shadow", "");
 		});
 
-		$(this.id).on("click", function(event) {
+
+		$(this.id).on("click", function() {
 			for (var i = 0; i < self.contents.length; ++i) {
 				console.log(self.contents[i]);
 			}
 		});
+
+
+		// this event is responsible for when a button is being pressed, as the button is being pressed
+		// you can change the design to make it look like its being pressed
+		$(this.id).on("mousedown", function() {
+			$(self.id).css("bottom", "-4px");
+			$(self.id).css("box-shadow", "0 1px 0 #00823F");
+		});
+
+		// this event is the opposite of mousedown, when the button gets released this event is fired, also
+		// change some css to make it look like button is being released, or simply revert back to the old style upon release
+		$(this.id).on("mouseup", function() {
+			$(self.id).css("box-shadow", "0 4px 2px -2px gray"); // shadow of the box on focus
+			$(self.id).css("bottom", "+4px"); // moves the positiion back to normal on focus
+		});
+
+
 	}
 
 	// the following method is a setter for the contents attribute
@@ -60,20 +78,19 @@ class Download {
 	}
 
 	generateCSS() {
-		var id = this.id;				
-		$(id).css("background-image", this.background);
-		$(id).css("background-repeat", "no-repeat");
-		$(id).css("background-size", "contain");
-		$(id).css("background-position", "center");
+		var id = this.id;	
+		var p = "#description";			
 		$(id).css("position", "relative");
-		$(id).css("border-radius", "80%");
-		$(id).css("border", "0"); // removes the black outline on click
-		$(id).css("outline", "none");
-		$(id).css("cursor", "pointer");
 		$(id).css("top", this.y);
 		$(id).css("left", this.x);
+		$(id).css("font-size", "105%");
+		$(id).css("text-align", "center");
 		$(id).css("width", this.width);
 		$(id).css("height", this.height);
+		$(id).css("cursor", "pointer");
+		$(p).css("position", "relative");
+		$(p).css("top", "10px");
+		$(p).css("text-shadow", "1px 0 white, 0 0px black, 0px 0 black, 0 1px white");
 	}
 
 
