57a85db880fac438fc6e56e053c326843a605642
tanvirtin <tanvir.tinz@gmail.com>
2020-12-07 23:21:29 -0500

    remove unncessary resolvers

--- api/src/file/file.module.ts
+++ api/src/file/file.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileEntity } from './file.entity';
-import { FileResolver } from './file.resolver';
 
 @Module({
     imports: [TypeOrmModule.forFeature([FileEntity])],
-    providers: [FileResolver],
 })
 export class FileModule {}
--- api/src/file/file.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver, Query } from '@nestjs/graphql';
-
-@Resolver()
-export class FileResolver {
-    @Query(() => String)
-    async hello() {
-        return 'Hello World';
-    }
-}
