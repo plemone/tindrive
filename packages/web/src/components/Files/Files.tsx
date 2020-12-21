30e2bbdd1dd82d77d795ada9b99018e0f95b3d71
tanvirtin <tanvir.tinz@gmail.com>
2020-12-21 18:58:35 -0500

    shorted no content window

--- packages/web/src/components/Files/Files.tsx
+++ packages/web/src/components/Files/Files.tsx
@@ -40,7 +40,7 @@ export const useStyles = makeStyles(theme => ({
         flexWrap: 'wrap',
         overflowY: 'auto',
         minWidth: 300,
-        minHeight: 150,
+        minHeight: 140,
         paddingLeft: theme.spacing(2),
         paddingTop: theme.spacing(2),
         paddingBottom: theme.spacing(2),
