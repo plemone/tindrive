1cd1eadf4283b52f8257d809e10d0eee7af1f8e4
tanvirtin <tanvir.tinz@gmail.com>
2020-12-24 17:18:48 -0500

    properly fix the useRouter mock

--- packages/web/src/components/Files/Files.test.tsx
+++ packages/web/src/components/Files/Files.test.tsx
@@ -5,49 +5,46 @@ import { MockedProvider } from '@apollo/client/testing';
 import { ls } from '../../queries';
 import Files from '.';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const useRouter = jest.spyOn(require('next/router'), 'useRouter');
-
-const data = [
-    {
-        name: 'dotfiles',
-        path: './dotfiles',
-        extension: null,
-        isDirectory: true,
-        parentDirectory: '.',
-        createdDate: '2020-11-05T03:03:46.950Z',
-        size: 4096,
-        populatedDate: '2020-12-13T18:10:38.000Z',
-    },
-    {
-        name: 'interview',
-        path: './interview',
-        extension: null,
-        isDirectory: true,
-        parentDirectory: '.',
-        createdDate: '2020-11-05T05:34:51.219Z',
-        size: 4096,
-        populatedDate: '2020-12-13T18:10:38.000Z',
-    },
-];
-
-const Wrapper: React.FC = () => (
-    <MockedProvider
-        addTypename={false}
-        mocks={[{
-            request: {
-                query: ls,
-                variables: { path: './' },
-            },
-            result: { data: { ls: data } },
-        }]}
-    >
-        <Files />
-    </MockedProvider>
-);
-
 describe('Files', () => {
-    beforeAll(() => useRouter.mockImplementationOnce(() => ({ query: {} })));
+    const data: {}[] = [
+        {
+            name: 'dotfiles',
+            path: './dotfiles',
+            extension: null,
+            isDirectory: true,
+            parentDirectory: '.',
+            createdDate: '2020-11-05T03:03:46.950Z',
+            size: 4096,
+            populatedDate: '2020-12-13T18:10:38.000Z',
+        },
+        {
+            name: 'interview',
+            path: './interview',
+            extension: null,
+            isDirectory: true,
+            parentDirectory: '.',
+            createdDate: '2020-11-05T05:34:51.219Z',
+            size: 4096,
+            populatedDate: '2020-12-13T18:10:38.000Z',
+        },
+    ];
+
+    const Wrapper: React.FC = () => (
+        <MockedProvider
+            addTypename={false}
+            mocks={[{
+                request: {
+                    query: ls,
+                    variables: { path: './' },
+                },
+                result: { data: { ls: data } },
+            }]}
+        >
+            <Files />
+        </MockedProvider>
+    );
+
+    beforeAll(() => jest.mock('next/router', () => ({ useRouter: (): {} => ({ query: { path: './' } }) })));
 
     test('render', async () => {
         const { getByTestId } = render(<Wrapper />);
